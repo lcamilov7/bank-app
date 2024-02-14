11323878cde2abd2d958170bb3d50f98eb23990f
Luis Camilo <luiscamilov@hotmail.com>
2024-02-13 21:28:31 -0500

    tratando de hacer el edit de clientes

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,6 @@
 class ClientsController < ApplicationController
+  before_action :set_client, only: %i[show edit update destroy]
+
   def index
   end
 
@@ -12,30 +14,35 @@ class ClientsController < ApplicationController
 
   def create
     @account = Account.new(account_params)
-    @account.number = '12345'
+    @account.number = Faker::Bank.account_number
     @account.save
+    # Creamos intancia de client usando los params que llegan de la vista
+    # y asignamos la cuenta recien creada @account como asociación
     @client = Client.new(client_params.merge(account: @account))
     @client.save
 
-
-    redirect_to root_path
-    # if @client.save
-    #   redirect_to banks_url, notice: 'Client created'
-    # else
-    #   render :new, status: :unprocessable_entity
-    # end
+    redirect_to root_path, notice: 'Client created'
   end
 
   def edit
+    @account = @client.account
   end
 
   def update
+    @account.update(account_params)
+    @client.update(client_params.merge(account: @account))
+
+    redirect_to root_path
   end
 
   def destroy
   end
 
   private
+
+  def set_client
+    @client = Client.find(params[:id])
+  end
 
   def client_params
     params.require(:client).permit(:name, :document_type, :document_number, :phone)
