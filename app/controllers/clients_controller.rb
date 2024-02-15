008552a147dc88fe089844ec306d82ef6c35beb1
Luis Camilo <luiscamilov@hotmail.com>
2024-02-15 10:13:23 -0500

    antes de comenzar edit en clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,6 +2,7 @@ class ClientsController < ApplicationController
   before_action :set_client, only: %i[show edit update destroy]
 
   def index
+    @clients = Client.all
   end
 
   def show
@@ -13,37 +14,19 @@ class ClientsController < ApplicationController
   end
 
   def create
-    # @account = Account.new(account_params)
-    # @account.number = Faker::Bank.account_number
-    # @account.save
-    # # Creamos instancia de client usando los params que llegan de la vista
-    # # y asignamos la cuenta recién creada @account como asociación
-    # @client = Client.new(client_params.merge(account: @account))
-
-    # if @client.save
-    #   redirect_to root_path, notice: 'Client created'
-    # else
-    #   render :new, status: :unprocessable_entity
-    # end
-
     @client = Client.new(client_params)
     @client.account.number = Faker::Bank.account_number
     if @client.save!
-      redirect_to root_path
+      redirect_to bank_path(@client.bank), notice: 'Client created'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @account = @client.account
   end
 
   def update
-    @account.update(account_params)
-    @client.update(client_params.merge(account: @account))
-
-    redirect_to root_path
   end
 
   def destroy
@@ -58,8 +41,4 @@ class ClientsController < ApplicationController
   def client_params
     params.require(:client).permit(:name, :document_type, :document_number, :phone, account_attributes: [:bank_id])
   end
-
-  # def account_params
-  #   params[:client].require(:account).permit(:bank_id)
-  # end
 end
